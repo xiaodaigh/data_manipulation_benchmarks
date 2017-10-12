1eb81b94ecd1932d98d8c94edde4878dedc4434f
susabi <zhuojia.dai@gmail.com>
2017-10-13 00:00:34 +1100

    minor edits

--- Julia/1_synthetic_aggregation.jl
+++ Julia/1_synthetic_aggregation.jl
@@ -1,13 +1,14 @@
 using Feather, DataFrames, BenchmarkTools
+
+@benchmark aggregate(+, DT, by=(:id1,), with=:v1)
+
 # about 1min
 #@time const DT = Feather.read(data_path * "/dt.feather")
-aggr(dt) = by(dt,:id1, subdt -> sum(subdt[:v1]))
-aggr2(dt) = aggregate(dt[:,[:id1,:v1]], :id1, sum)
-
-@benchmark aggr2(DT)
-@benchmark aggr2(DT)
-
-@benchmark aggr(DT)
-@benchmark aggr(DT)
-
-@time readstring(`go run D:/git/julia_r_python_ge_flight_quest/Julia/main.go`)
+# aggr(dt) = by(dt,:id1, subdt -> sum(subdt[:v1]))
+# aggr2(dt) = aggregate(dt[:,[:id1,:v1]], :id1, sum)
+#
+# @benchmark aggr2(DT)
+# @benchmark aggr2(DT)
+#
+# @benchmark aggr(DT)
+# @benchmark aggr(DT)
